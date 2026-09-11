ac13c1554627f7396fce184e4d99edad8d2753ba
kiem.nguyen <nguyenhaanhkiem@gmail.com>
2026-09-11 21:44:50 +0700

    feat: add feature form constants and export barrel file for scraping configuration modules

--- src/app/(root)/scraping/features/constants/feature-form.constants.ts
+++ src/app/(root)/scraping/features/constants/feature-form.constants.ts
@@ -1,8 +1,8 @@
 import { DataProviderFeatureType } from '../enums';
 import type { FormEvaluationContext, FormSectionSchema } from '../types';
 import { getDefaultFormValuesFromSections } from '../utils';
-import { SCRAPING_FORM_SECTIONS } from './scraping.constants';
-import { SEARCH_FORM_SECTIONS } from './search.constants';
+import { SCRAPING_FORM_SECTIONS } from './scraping-config.constants';
+import { SEARCH_FORM_SECTIONS } from './search-config.constants';
 
 export const FEATURE_FORM_SECTIONS: Record<DataProviderFeatureType, FormSectionSchema[]> = {
     [DataProviderFeatureType.SCRAPING]: SCRAPING_FORM_SECTIONS,
--- src/app/(root)/scraping/features/constants/index.ts
+++ src/app/(root)/scraping/features/constants/index.ts
@@ -1,5 +1,5 @@
 export * from './common.constants';
 export * from './feature-form.constants';
-export * from './scraping.constants';
-export * from './search.constants';
+export * from './scraping-config.constants';
+export * from './search-config.constants';
 export * from './feature-status.constants';
